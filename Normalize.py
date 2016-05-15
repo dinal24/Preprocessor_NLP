98f9aa0afb6b8dea1a0fff409f76b767d819d187
dinal24 <kdinal24@gmail.com>
2016-05-15 16:52:49 +0530

    to lower caps with Unicode

--- Normalize.py
+++ Normalize.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 __author__ = 'dinal'
-
 
 def removeRedundantWhiteSpaces(str_input):
     ret = " ".join(str_input.split())
     return ret
+
+def toLowerCase(str_input):
+    return str_input.decode('utf-8').lower()
+
+
 
--- Normalize_Test.py
+++ Normalize_Test.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
 __author__ = 'dinal'
 
 import unittest
 from Normalize import removeRedundantWhiteSpaces
+from Normalize import toLowerCase
 
 class TestNormalize(unittest.TestCase):
-    def test_space(self):
+    def test_removeRedundantWhiteSpaces(self):
         self.assertEqual(removeRedundantWhiteSpaces(" \t f\t\t   o o\t"), 'f o o')
-
-    def test_newline(self):
         self.assertEqual(removeRedundantWhiteSpaces('\nf\n\n\t\r\r\ro\t o'),'f o o')
 
+    def test_toLowerCase(self):
+        self.assertEqual(toLowerCase('ABCDefg'), 'abcdefg')
+
+    def test_toLowerCase_NonASCII(self):
+        self.assertEqual(toLowerCase('Километ'), 'километ'.decode('utf-8'))
 
 
 if __name__ == '__main__':
