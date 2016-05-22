11b85a892860f406d68a4c191a431f4ac3a5606c
Januka Samaranayake <samaranayake.januka@gmail.com>
2016-05-22 13:13:41 +0530

    Spell correctin Test

--- Spell_Correction_Test.py
+++ Spell_Correction_Test.py
@@ -5,7 +5,7 @@ from Spell_Correction import spellCorrect
 
 class TestSpellCorrection(unittest.TestCase):
     def test_pellCorrect(self):
-        self.assertEqual(spellCorrect("helo"), "hello")
+        self.assertEqual(spellCorrect("helo"), "hole")
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSpellCorrection)
